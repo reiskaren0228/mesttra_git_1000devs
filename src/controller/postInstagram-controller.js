2390882f64973e5ca1bd02b907ea4a98cce3fcc2
reiskaren <kreisdemenezes@gmail.com>
2023-12-07 23:12:43 -0300

    add projeto github

---
+++ src/controller/postInstagram-controller.js
@@ -0,0 +1,7 @@
+function instagram(){
+    for(let idx = 0; idx<10; idx++){
+        console.log('instagram');
+    }
+}
+
+console.log("produtos de beleza mais procurados.");
---
+++ src/controller/user-controller.js
@@ -0,0 +1,17 @@
+const calculaIdade = (req, res) => {
+    const hoje = new Date(Date.now())
+    const nascimento = new Date("1983-02-14")
+    const idade = hoje.getFullYear() - nascimento.getFullYear()
+
+    res.json({
+        "nome": "leonardo",
+        "cidade": "Vitoria",
+        "UF": "ES",
+        "profissao": "programador",
+        "nascimento": idade,
+    })
+}
+
+module.exports = {
+    calculaIdade
+}
